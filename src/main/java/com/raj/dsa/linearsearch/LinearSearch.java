ce76a682b18ea488de8094b34c9f0636f798c3a1
Rajkumar <rajsennimalai@users.noreply.github.com>
2022-04-17 16:04:02 +0530

    Linear search implemented with Junit5

--- src/main/java/com/raj/dsa/linearsearch/LinearSearch.java
+++ src/main/java/com/raj/dsa/linearsearch/LinearSearch.java
@@ -1,11 +1,27 @@
 package com.raj.dsa.linearsearch;
 
+import java.util.List;
+
 /**
- * input: List<String> numbers, int numberToFind
- * Output: number
+ * input: List<int> numbers, int target
+ * Output: Returns the index of the target element else -1
  */
 public class LinearSearch {
 
-	
+	public int linearSearch(List<Integer> list, int target) {
+		int result=-1;
+		int lengthOfList=list.size();
+		
+		for(int i=0;i<lengthOfList;i++) {
+			if(list.get(i).equals(target)) {
+				result=i;
+				break;
+			}
+		}
+		
+		
+		return result;
+		
+	}
 	
 }
